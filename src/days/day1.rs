4cf966667a1368999c7171ce3f1975890b31e6ab
Jeremy Haak <jeremy.haak@cronegeophysics.com>
2021-12-01 09:40:56 -0500

    Add tests and refactor to allow testing

--- src/days/day1.rs
+++ src/days/day1.rs
@@ -1,18 +1,42 @@
-use crate::tools::get_input;
-
-pub fn a() -> usize {
-    let numbers: Vec<u64> = get_input(1)
-        .lines()
-        .filter_map(|line| line.parse().ok())
-        .collect();
+pub fn a(input: &str) -> usize {
+    let numbers = parse(input);
     numbers.windows(2).filter(|w| w[1] > w[0]).count()
 }
 
-pub fn b() -> usize {
-    let numbers: Vec<u64> = get_input(1)
-        .lines()
-        .filter_map(|line| line.parse().ok())
-        .collect();
+pub fn b(input: &str) -> usize {
+    let numbers = parse(input);
     let windows: Vec<u64> = numbers.windows(3).map(|w| w.iter().sum()).collect();
     windows.windows(2).filter(|w| w[1] > w[0]).count()
+}
+
+fn parse(input: &str) -> Vec<u64> {
+    input
+        .lines()
+        .filter_map(|line| line.trim().parse().ok())
+        .collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const INPUT: &str = "199
+                         200
+                         208
+                         210
+                         200
+                         207
+                         240
+                         269
+                         260
+                         263";
+    #[test]
+    fn part_a() {
+        assert_eq!(a(INPUT), 7);
+    }
+
+    #[test]
+    fn part_b() {
+        assert_eq!(b(INPUT), 5);
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
 use aoc2021::days::*;
+use aoc2021::tools::get_input;
 
 fn main() {
-    println!("Day 1: {}, {}", day1::a(), day1::b());
+    let solutions = [(day1::a, day1::b)];
+
+    for (day, (a, b)) in solutions.iter().enumerate() {
+        let day = day + 1;
+        let data = get_input(day);
+        println!("Day {}: {}, {}", day, a(&data), b(&data));
+    }
 }
