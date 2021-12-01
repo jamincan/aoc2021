7487f592f71b78b883b574d5c4738fc8fad61ca9
Jeremy Haak <jeremy.haak@cronegeophysics.com>
2021-12-01 09:40:56 -0500

    Add tests and refactor to allow testing

--- src/tools.rs
+++ src/tools.rs
@@ -1,4 +1,4 @@
-pub fn get_input(day: u8) -> String {
+pub fn get_input(day: usize) -> String {
     let filename = format!("./assets/day{}.txt", day);
     std::fs::read_to_string(filename).unwrap()
 }
